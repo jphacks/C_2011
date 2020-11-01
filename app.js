9ddaa42640bbad887743f476aed31c15526b9b3a
oharabutyou <oharabutyou@gmail.com>
2020-11-01 23:12:38 +0900

    change app.js, add empty ejs files

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+    res.render('hello.ejs');
+});
+
+app.get('/rec_list', (req, res) => {
+    res.render('rec_list.ejs');
+});
+
+app.get('/sns_list', (req, res) => {
+    res.render('sns_list.ejs');
+});
+
+app.get('/timer', (req, res) => {
+    res.render('timer.ejs');
+});
+
+app.get('/camera', (req, res) => {
+    res.render('camera.ejs');
+});
+
+app.get('/goal_list', (req, res) => {
+    res.render('goal_list.ejs');
 });
 
 // サーバーを起動するコードを貼り付けてください
