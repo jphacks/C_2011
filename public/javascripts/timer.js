fc792827d27bc8c5b9675b2a7c66ea1c1a264cce
haru1074 <hunter-revolu.636-1123@docomo.ne.jp>
2020-11-02 21:04:20 +0900

    modify timer.js

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,10 +1,20 @@
 $("#timer-btn").click(function() {
+
+    // 開始ボタンの時の処理
     if($(this).hasClass("timer-start")) {
+        // 現在時刻取得
+        var date = new Date()
+        console.log(date.getTime())
+
+        // クラス変更
         $(this).addClass("timer-stop");
         $(this).removeClass("timer-start");
         $(this).text("停止");
-        console.log("aaaaa");
-    } else {
+    } 
+
+    // 停止ボタンの時の処理
+    else {
+        // クラス変更
         $(this).addClass("timer-start");
         $(this).removeClass("timer-stop");
         $(this).text("開始");
