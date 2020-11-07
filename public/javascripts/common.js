270e8762c87c3dfdaad216305d720edc203501bd
terutika0604 <terutika0604@gmail.com>
2020-11-07 13:05:29 +0900

    ローディング画面＆ロゴ実装

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,5 +1,22 @@
 $(function(){
 
+
+  function stopload() {
+    $('#loading-wrap').delay(900).fadeOut(800);
+    $('html,body').css('overflow', 'visible');
+  }
+
+  $('html,body').css('overflow', 'hidden');
+
+  $(window).load(function () {
+    stopload();
+    });
+
+  setTimeout(function(){
+		$('#loading-wrap .img').fadeIn(1000);
+	},500);
+
+
   // メニュー開閉
   function menuOpen() {
     $('.menu').toggleClass('menu-close');
